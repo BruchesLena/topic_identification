8341d448e08782ef2d381f96c06c4fec9d004bf5
BruchesElena <bruches@bk.ru>
2018-12-12 12:34:43 +0400

    Sentiment for English

---
+++ sentiment_demo/example_eng.py
@@ -0,0 +1,8 @@
+from predictor_eng import Predictor
+
+predictor = Predictor()
+while True:
+    text = input('text:\n')
+    predictions = predictor.predict(text)
+    for p in predictions:
+        print(p[0], str(p[1]))
